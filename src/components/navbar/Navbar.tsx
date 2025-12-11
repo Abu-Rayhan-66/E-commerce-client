d2982bac20d0c5bdb750fd8465aba9769292a5ee
abu-rayhan66 <mraburayhan66@gmail.com>
2025-12-11 12:34:17 +0600

    make some changes in nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -40,6 +40,9 @@ const Navbar = () => {
         <button className="px-3 py-1 border border-black rounded text-sm">
           Report Problem
         </button>
+        <button className="px-3 py-1 border border-black rounded text-sm">
+          Login
+        </button>
       </div>
     </nav>
         </div>
