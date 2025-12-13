02406ccd9f5598862d79316ff67c239f72fb59bd
abu-rayhan66 <mraburayhan66@gmail.com>
2025-12-13 13:54:34 +0600

    make some changes in nav

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -44,6 +44,9 @@ const Navbar = () => {
           Fast Shipping
         </button>
         <button className="px-3 py-1 border border-black rounded text-sm">
+          Return policy
+        </button>
+        <button className="px-3 py-1 border border-black rounded text-sm">
           Login
         </button>
       </div>
