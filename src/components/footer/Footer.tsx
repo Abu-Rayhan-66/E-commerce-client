fcbaec3f9762a43f7af0c2010ab4a765439c6113
abu-rayhan66 <mraburayhan66@gmail.com>
2026-01-17 18:19:09 +0600

    make some changes in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -55,6 +55,7 @@ const Footer = () => {
               <li><a href="#" className="hover:text-indigo-400 transition">Up Coming</a></li>
               <li><a href="#" className="hover:text-indigo-400 transition">Trending</a></li>
               <li><a href="#" className="hover:text-indigo-400 transition">Best Seller</a></li>
+              <li><a href="#" className="hover:text-indigo-400 transition">Best Category</a></li>
           
 
               
